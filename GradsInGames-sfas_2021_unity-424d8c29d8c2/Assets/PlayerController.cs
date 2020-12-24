b0698e04773e64a88c6a406acf293faccc18870c
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-12-24 16:02:08 +0000

    animation rigging

--- GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/PlayerController.cs
+++ GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/PlayerController.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float _movementSpeed;
+    public Vector3 point;
+
     private Rigidbody _RB;
+    private Animator _Animator;
+
     // Start is called before the first frame update
     void Start()
     {
         _RB = GetComponent<Rigidbody>();
+        _Animator = GetComponent<Animator>();
     }
-    public Vector3 point;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        _Animator.SetFloat("Xmove", move.x);
+        _Animator.SetFloat("Ymove", move.z);
 
         //Get the Screen position of the mouse
         Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -42,5 +49,54 @@ public class PlayerController : MonoBehaviour
 
         //_RB.MovePosition(transform.position + Time.deltaTime * _movementSpeed * transform.TransformDirection(move));
         _RB.velocity = move;
+    }
+
+
+    public static Vector2 CurrentDirection(Vector2 input, GameObject agent)
+    {
+        var a = Vector3.SignedAngle(new Vector3(input.x, 0, input.y), agent.transform.forward, agent.transform.up);
+
+        // Normalize the angle
+        if (a < 0)
+        {
+            a *= -1;
+        }
+        else
+        {
+            a = 360 - a;
+        }
+
+        // Take into consideration the angle of the camera
+        //a += Camera.main.transform.eulerAngles.y;
+
+        var aRad = Mathf.Deg2Rad * a; // degrees to radians
+                                      //Debug.Log(leftStickInputAxis);
+                                      // If there is some form of input, calculate the new axis relative to the rotation of the model
+        if (input.x != 0 || input.y != 0)
+        {
+            input = new Vector2(Mathf.Sin(aRad), Mathf.Cos(aRad));
+        }
+
+
+        if (input.x < -1)
+        {
+            input.x = -1;
+        }
+        else if (input.x > 1.6f)
+        {
+            input.x = 1.6f;
+        }
+
+
+        if (input.y < -1)
+        {
+            input.y = -1;
+        }
+        else if (input.y > 1.6f)
+        {
+            input.y = 1.6f;
+        }
+
+        return input;
     }
 }
